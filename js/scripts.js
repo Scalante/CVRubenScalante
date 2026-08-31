d2426cf0a4e2ea743c2676e916473b8c0cb1b591
Scalante <rubendarioscalante@gmail.com>
2026-08-31 12:50:18 -0500

    feat: modernize portfolio UI/UX, add i18n, and update CV details

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleTab(tabId) {
-    var content = document.getElementById(tabId);
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
